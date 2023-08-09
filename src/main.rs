d6e235b959ba9aa7185ec0b10c0b8ae65c51a23c
Burak Güner <burak.guner@callstack.com>
2023-08-10 00:57:13 +0300

    feat: add socket handler

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use actix::{Actor, StreamHandler};
 use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer};
 use actix_web_actors::ws;
+use bytestring::ByteString;
+use curver_backend::point::Point;
 
 /// Define HTTP actor
 struct MyWs;
@@ -9,18 +11,29 @@ impl Actor for MyWs {
     type Context = ws::WebsocketContext<Self>;
 }
 
+fn handle_string(s: ByteString) -> Option<String> {
+    let point = serde_json::from_str::<Point>(&s).ok()?;
+    let result = point.x + point.y;
+    Some(result.to_string())
+}
+
 /// Handler for ws::Message message
 impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
     fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
         match msg {
-            Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
-            Ok(ws::Message::Text(text)) => ctx.text(text),
-            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
-            _ => (),
+            Ok(ws::Message::Text(text)) => {
+                let result = handle_string(text);
+                match result {
+                    Some(s) => ctx.text(s),
+                    None => ctx.text("Error"),
+                }
+            }
+            _ => ctx.text("Error"),
         }
     }
 }
 
+#[actix_web::get("/ws")]
 async fn index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
     let resp = ws::start(MyWs {}, &req, stream);
     println!("{:?}", resp);
@@ -29,7 +42,7 @@ async fn index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, E
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/ws/", web::get().to(index)))
+    HttpServer::new(|| App::new().service(index))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
